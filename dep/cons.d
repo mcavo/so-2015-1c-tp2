af49f1bc5ecdae96b46ec5d277c11c34f7e04dfd
Vicky <mvictoria.cavo@gmail.com>
2015-06-27 17:39:26 -0300

    lala

--- dep/cons.d
+++
@@ -1,3 +0,0 @@
-obj/cons.o dep/cons.d: src/drivers/cons.c include/kernel.h \
- include/mtask.h include/lib.h include/segments.h include/const.h \
- include/drivers.h
--- dep/malloc.d
+++
@@ -1,3 +0,0 @@
-obj/malloc.o dep/malloc.d: src/lib/malloc.c include/kernel.h \
- include/mtask.h include/lib.h include/segments.h include/const.h \
- include/drivers.h
